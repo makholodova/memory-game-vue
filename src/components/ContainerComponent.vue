0f88891fdf362f03b0f411894e5b51635defa877
Marina Kholodova <marinaholodova@gmail.com>
2024-10-07 21:52:55 +0300

    feat: add game info and container components

---
+++ src/components/ContainerComponent.vue
@@ -0,0 +1,45 @@
+﻿<template>
+  <div id="inner__container" class="container content">
+      <slot></slot>
+  </div>
+</template>
+<script lang="ts" setup>
+</script>
+<style scoped lang="scss">
+.container {
+  width: 100%;
+  max-width: 980px;
+  padding: 0 40px;
+  margin: 0 auto;
+
+  @include tablets {
+    padding: 0 30px;
+  }
+  @include phones {
+    padding: 0 10px;
+  }
+}
+
+.content {
+  /*padding-top: 20px;
+  padding-bottom: 30px;*/
+  /*padding: 20px 40px 30px 40px;*/
+  height: 100%;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  max-width: 920px;
+
+  background: rgba(255, 255, 255, 0.3);
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.2);
+
+  border-radius: 20px;
+  @include phones {
+    padding-top: 10px;
+    padding-bottom: 20px;
+  }
+}
+</style>
---
+++ src/components/GameInfo.vue
@@ -0,0 +1,21 @@
+﻿<template>
+  <div class="game-info">
+    <ScoreComponent/>
+    <TimerComponent/>
+  </div>
+</template>
+<script lang="ts" setup>
+
+import ScoreComponent from "@/components/ScoreComponent.vue";
+import TimerComponent from "@/components/TimerComponent.vue";
+</script>
+
+<style scoped>
+.game-info {
+  display: flex;
+  justify-content: space-between;
+  max-width: 750px;
+  width: 100%;
+  margin-bottom: 20px;
+}
+</style>
