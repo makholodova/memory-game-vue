7d8485870db0d8c654e69eff02eeca049f36ced9
Marina Kholodova <marinaholodova@gmail.com>
2024-10-07 21:52:10 +0300

    feat:add ScoreComponent

---
+++ src/components/ScoreComponent.vue
@@ -0,0 +1,28 @@
+﻿<template>
+  <div id="score" class="score-display">Score: {{ score }}
+  </div>
+</template>[
+<script lang="ts" setup>
+import {ref} from "vue";
+
+let score = ref<number>(0);
+
+const increaseScore = (): void => {
+  score.value++;
+}
+</script>
+
+<style scoped lang="scss">
+.score-display {
+  line-height: 1rem;
+  background-color: rgba(255, 121, 176, 0.3);
+  padding: 10px;
+  border: 2px solid #ff79b0;
+  border-radius: 10px;
+  @include phones {
+    font-size: 10px;
+    line-height: 1;
+    padding: 5px;
+  }
+}
+</style>
