165e1021ec7fc158e7a42ede7b4c1f3e63712c41
Marina Kholodova <marinaholodova@gmail.com>
2024-10-07 21:53:24 +0300

    feat: add footer

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,18 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts" setup>
+import {RouterView} from 'vue-router'
+import FooterComponent from "@/components/FooterComponent.vue";
 </script>
 
 <template>
   <header>
-<!--
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
--->
-
-    <div class="wrapper">
-<!--      <HelloWorld msg="You did it!" />-->
-
-      <nav>
-<!--        <RouterLink to="/">Home</RouterLink>-->
-<!--        <RouterLink to="/about">About</RouterLink>-->
-      </nav>
-    </div>
   </header>
 
-  <RouterView />
+  <RouterView/>
+  <FooterComponent/>
 </template>
 
 <style scoped>
-header {
+/*header {
   line-height: 1.5;
   max-height: 100vh;
 }
@@ -83,5 +72,5 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
-}
+}*/
 </style>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,60 @@
+﻿<script lang="ts" setup>
+
+</script>
+
+<template>
+  <footer class="footer">
+    <div class="footer__container container">
+      <a class="footer__link" href='https://github.com/makholodova' target="_blank"> <!--/*link*/-->
+        <img alt="github" class="footer__icon" src="../assets/svg/simple-icons--github--footer.svg">
+        Visit my GitHub
+      </a>
+      <p class="footer__text">&copy; Marina Kholodova 2024 </p>
+      <a class="footer__link" href='https://rs.school/' target="_blank">
+        <img alt="logo_RS_School" class="footer__school-logo" src="../assets/svg/logo_rs_school.svg">
+      </a>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+.footer {
+  margin-top: 20px;
+  padding: 15px;
+  font-size: 16px;
+  color: $primary;
+}
+
+.footer__container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  @include phones {
+    flex-direction: column;
+    row-gap: 15px;
+  }
+}
+
+.footer__link {
+  color: $primary;
+  display: flex;
+  align-items: center;
+}
+
+.footer__icon {
+  width: 32px;
+  height: 32px;
+  margin-right: 10px;
+}
+
+.footer__school-logo {
+  width: 86px;
+  height: 32px;
+}
+
+.footer p {
+  margin: 0;
+}
+
+
+</style>
