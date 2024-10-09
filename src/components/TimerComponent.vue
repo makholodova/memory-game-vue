8ea11ef9cf0edc38f84fa4ed394c16004b6595f6
Marina Kholodova <marinaholodova@gmail.com>
2024-10-09 21:32:14 +0300

    feat: change timer

--- src/components/TimerComponent.vue
+++ src/components/TimerComponent.vue
@@ -1,56 +1,23 @@
 ﻿<template>
-  <div class="timer-display">Time: {{ formatTime(timeInSeconds) }}</div>
-  <!--  <button @click="startTimer">Start</button>
+  <div class="timer-display">Time: {{ timerStore.formatTime() }}</div>
+<!--    <button @click="startTimer">Start</button>
     <button @click="stopTimer">Stop</button>-->
 </template>
 
 <script lang="ts" setup>
-import {ref} from "vue";
+import {useTimerStore} from "@/store/timerStore";
 
-let timeInSeconds = ref<number>(0);
-let isRunning = ref<boolean>(false);
-let myInterval: number | undefined;
-
-const startTimer = () => {
-  if (isRunning.value) {
-    console.warn('Таймер уже запущен.');
-    return;
-  }
-  isRunning.value = true;
-  myInterval = window.setInterval(() => {
-    timeInSeconds.value++;
-  }, 1000);
-}
-
-const stopTimer = (): void => {
-  if (!isRunning.value) {
-    console.warn('Таймер уже остановлен.');
-    return;
-  }
-
-  clearInterval(myInterval);
-  myInterval = undefined;
-  isRunning.value = false;
-  timeInSeconds.value = 0;
-}
-
-const formatTime = (timeInSeconds: number): string => {
-  const minutes = Math.floor(timeInSeconds / 60);
-  const seconds = Math.floor(timeInSeconds % 60);
-  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
-  const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
-  return `${formattedMinutes}:${formattedSeconds}`;
-}
+const timerStore = useTimerStore();
 
 </script>
-<style scoped lang="scss">
+<style lang="scss" scoped>
 .timer-display {
   line-height: 1rem;
   background-color: rgba(255, 121, 176, 0.3);
   padding: 10px;
   border: 2px solid #ff79b0;
   border-radius: 10px;
-  
+
   @include phones {
     font-size: 10px;
     line-height: 1;
---
+++ src/store/timerStore.ts
@@ -0,0 +1,45 @@
+﻿import {defineStore} from "pinia";
+
+
+export const useTimerStore = defineStore('timer', {
+	state: () => ({
+		timeInSeconds: 0,
+		isRunning: false,
+		myInterval: undefined as number | undefined
+	}),
+	actions: {
+		startTimer() {
+			if (this.isRunning) {
+				console.warn('Таймер уже запущен.');
+				return;
+			}
+			this.isRunning = true;
+			this.myInterval = window.setInterval(() => {
+				this.timeInSeconds++;
+			}, 1000);
+		},
+
+		stopTimer(): void {
+			if (!this.isRunning) {
+				console.warn('Таймер уже остановлен.');
+				return;
+			}
+
+			clearInterval(this.myInterval);
+			this.myInterval = undefined;
+			this.isRunning = false;
+			this.timeInSeconds = 0;
+		},
+
+		formatTime(): string {
+			const minutes = Math.floor(this.timeInSeconds / 60);
+			const seconds = Math.floor(this.timeInSeconds % 60);
+			const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+			const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+			return `${formattedMinutes}:${formattedSeconds}`;
+		}
+
+
+	},
+});
+
